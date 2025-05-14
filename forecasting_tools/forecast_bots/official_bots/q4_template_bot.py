21db7976df7a1284abe6e22509c42de372e49364
MMX <mmx@MMXs-MacBook-Air.local>
2025-05-13 22:50:00 -0400

    Fix q4_template_bot.py to explicitly avoid AskNewsSearcher import

--- forecasting_tools/forecast_bots/official_bots/q4_template_bot.py
+++ forecasting_tools/forecast_bots/official_bots/q4_template_bot.py
@@ -1,15 +1,21 @@
+import logging
 from forecasting_tools.data_models.questions import MetaculusQuestion
 from forecasting_tools.forecast_bots.official_bots.q3_template_bot import (
     Q3TemplateBot2024,
 )
-from forecasting_tools.forecast_helpers.asknews_searcher import AskNewsSearcher
 
+# Explicitly NOT importing AskNewsSearcher since it has been removed
+# The import causing the error was:
+# from forecasting_tools.forecast_helpers.asknews_searcher import AskNewsSearcher
+
+logger = logging.getLogger(__name__)
 
 class Q4TemplateBot2024(Q3TemplateBot2024):
     """
-    Q4 Template Bot was the same as Q3 other than switching out for AskNews
+    Q4 Template Bot extends Q3 template with alternative research sources
     """
 
     async def run_research(self, question: MetaculusQuestion) -> str:
-        news = AskNewsSearcher().get_formatted_news(question.question_text)
-        return news
+        logger.info(f"Running research for question: {question.question_text}")
+        # Use the default research implementation from the parent class
+        return await super().run_research(question)
