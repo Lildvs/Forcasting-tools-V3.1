df264b1e37df1f4d5966c9ce211e808662126b8d
MMX <mmx@MMXs-MacBook-Air.local>
2025-05-13 22:52:03 -0400

    Fix q4_template_bot.py to remove AskNews import

--- forecasting_tools/forecast_bots/official_bots/q4_template_bot.py
+++ forecasting_tools/forecast_bots/official_bots/q4_template_bot.py
@@ -1,14 +1,8 @@
-import logging
 from forecasting_tools.data_models.questions import MetaculusQuestion
 from forecasting_tools.forecast_bots.official_bots.q3_template_bot import (
     Q3TemplateBot2024,
 )
 
-# Explicitly NOT importing AskNewsSearcher since it has been removed
-# The import causing the error was:
-# from forecasting_tools.forecast_helpers.asknews_searcher import AskNewsSearcher
-
-logger = logging.getLogger(__name__)
 
 class Q4TemplateBot2024(Q3TemplateBot2024):
     """
@@ -16,6 +10,5 @@ class Q4TemplateBot2024(Q3TemplateBot2024):
     """
 
     async def run_research(self, question: MetaculusQuestion) -> str:
-        logger.info(f"Running research for question: {question.question_text}")
         # Use the default research implementation from the parent class
         return await super().run_research(question)
