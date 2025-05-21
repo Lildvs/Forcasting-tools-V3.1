865e34c8fc82593f046db45820e5dc3dd7d517e9
Lildvs <mmoore18108@outlook.com>
2025-05-21 04:48:13 -0400

    Add 'Things to do that we know we will never get to' dropdown menu page

---
+++ pages/8_Things_to_never_get_to.py
@@ -0,0 +1,24 @@
+import sys
+import os
+import streamlit as st
+import dotenv
+
+# Add the parent directory to the path to import from forecasting_tools
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+# Import the NeverGetToPage
+from forecasting_tools.front_end.app_pages.never_get_to_page import NeverGetToPage
+
+# Set page config
+st.set_page_config(
+    page_title="Things to do that we know we will never get to - Forecasting Tools",
+    page_icon="🔮",
+    layout="wide",
+)
+
+# Load environment variables
+dotenv.load_dotenv()
+
+# Run the page
+if __name__ == "__main__":
+    NeverGetToPage.main() 
